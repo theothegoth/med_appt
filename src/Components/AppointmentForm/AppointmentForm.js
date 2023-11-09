3f68a1a8e6971cb0c03e8a90975bbfe4e2209729
theothegoth <huseyinguven24@hotmail.com>
2023-11-08 23:50:46 -0500

    initial commit

--- src/Components/AppointmentForm/AppointmentForm.js
+++ src/Components/AppointmentForm/AppointmentForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const AppointmentFormIC = ({ doctorName, doctorSpeciality, onSubmit }) => {
+const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
     const [name, setName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [selectedSlot, setSelectedSlot] = useState(null);
@@ -69,4 +69,4 @@ const AppointmentFormIC = ({ doctorName, doctorSpeciality, onSubmit }) => {
     );
   };
 
-export default AppointmentFormIC
+export default AppointmentForm
